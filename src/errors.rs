8d6b89b4c047733131b39de38ac388ce9159c33f
Abey Onalaja <Abey.onalaja@gmail.com>
2020-02-04 23:33:09 +0000

    update to errors

--- src/errors.rs
+++ src/errors.rs
@@ -1,5 +1,6 @@
 use actix_web::error::BlockingError;
 use actix_web::web::HttpResponse;
+use actix_web::App;
 use diesel::result::DatabaseErrorKind::UniqueViolation;
 use diesel::result::Error::{DatabaseError, NotFound};
 use std::fmt;
@@ -12,6 +13,11 @@ pub enum AppError {
     OperationCanceled,
 }
 
+#[derive()]
+struct ErrorResponse {
+    err: String,
+}
+
 impl fmt::Display for AppError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
@@ -20,5 +26,40 @@ impl fmt::Display for AppError {
             AppError::DatabaseError(e) => write!(f, "Database error: {:?}", e),
             AppError::OperationCanceled => write!(f, "The running operation was canceled"),
         }
+    }
+}
+
+impl From<diesel::result::Error> for AppError {
+    fn from(e: diesel::result::Error) -> Self {
+        match e {
+            DatabaseError(UniqueViolation, _) => AppError::RecordAlreadyExists,
+            NotFound => AppError::RecordNotFound,
+            _ => AppError::DatabaseError(e),
+        }
+    }
+}
+
+impl From<BlockingError<AppError>> for AppError {
+    fn from(e: BlockingError<AppError>) -> Self {
+        match e {
+            BlockingError::Error(inner) => inner,
+            BlockingError::Canceled => AppError::OperationCanceled,
+        }
+    }
+}
+
+impl actix_web::ResponseError for AppError {
+    fn error_response(&self) -> HttpResponse {
+        let err = format!("{}", self);
+        let mut builder = match self {
+            AppError::RecordAlreadyExists => HttpResponse::BadRequest(),
+            AppError::RecordNotFound => HttpResponse::NotFound(),
+            _ => HttpResponse::InternalServerError(),
+        };
+        builder.json(ErrorResponse { err })
+    }
+
+    fn render_response(&self) -> HttpResponse {
+        self.error_response()
     }
 }
